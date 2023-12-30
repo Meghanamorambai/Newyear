df4346139506c2078f997ecc7351c4428d9312bc
Meghanamorambai <153491917+Meghanamorambai@users.noreply.github.com>
2023-12-30 14:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,214 @@
+
+*{
+    padding: 0;
+    margin: 0;
+}
+body{
+    background-color: #121212;
+    font-family: 'Montserrat', sans-serif;
+}
+.sidebar {
+    position: fixed;
+    
+    width: 196px;
+    background-color: #000000;
+    padding: 24px;}
+
+.sidebar .navigation ul{
+    list-style:  none;
+    margin-top: 20px;
+}
+.sidebar .navigation ul li{
+    padding: 10px 0px;
+}
+.sidebar .navigation ul li a{
+    color: #b3b3b3;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 13px;
+
+}
+.sidebar .policies ul li a:hover,
+.sidebar .policies ul li a:active,
+.sidebar .policies ul li a:focus {
+    color: #ffffff;
+}
+
+.sidebar .policies ul li a:hover,
+.sidebar .policies ul li a:active,
+.sidebar .policies ul li a:focus {
+    color: #b3b3b3;
+}
+.sidebar .navigation ul li .fa{
+    font-size: 20px;
+    margin-left: 10px;
+}
+.sidebar .policies ul li a:hover.fa:hover,
+.sidebar .policies ul li a:hover.fa:active,
+.sidebar .policies ul li a:hover.fa:focus{
+    color: #ffffff;
+}
+sidebar .policies {
+    position: absolute;
+    bottom: 100px;
+  }
+  .sidebar .policies ul {
+    list-style: none;
+  }
+  .sidebar .policies ul li {
+    padding-bottom: 5px;
+  }
+  .sidebar .policies ul li a {
+    color: #b3b3b3;
+    font-weight: 500;
+    text-decoration: none;
+  font-size: 10px;
+}
+.sidebar .policies ul li a:hover,
+.sidebar .policies ul li a:active,
+.sidebar .policies ul li a:focus {
+  text-decor
+<div class="sidebar">
+    <div class="logo">
+      <a href="#">
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png"
+         alt="Logo" style="width: 50px;height: 30px;"></div>
+        </a>
+    </div>
+
+
+
+
+
+
+
+    .spotify-playlists{
+    padding: 20px 40px;
+}
+.spotify-playlists h2{
+color: #ffffff;
+font-size: 22px;
+margin-bottom: 20px;
+}
+.spotify-playlists .list{
+    display: flex;
+    font-size: 22px;
+    margin-bottom: 20px;
+}
+.spotify-playlists .list .item{
+    min-width: 140px;
+    width: 160px;
+    padding: 15px;
+    background-color: #181818;
+    border-radius: 46px;
+    cursor: pointer;
+    transition: all ease 0.4s;
+}
+.spotify-playlists .list .item:hover{
+    background-color: #252525;
+}
+.spotify-playlists .list .item img{
+    width: 100%;
+    border-radius: 6px;
+    margin-bottom: 10px;
+}
+.spotify-playlists .list .item .play{
+    position: relative;
+}
+.spotify-playlists .list .item .play .fa{
+    position: absolute;
+    right: 10px;
+    top: -60px;
+    padding: 18px;
+    background-color: #1db954;
+    border-radius: 100%;
+    opacity: 0;
+    transition: all ease 0.4s;
+}
+.spotify-playlists .list .item:hover .play .fa{
+    opacity: 1;
+    transform: translateY(-20px);
+}
+ .spotify-playlists .list .item h4{
+    color: #ffffff;
+    font-size: 14px;
+    margin-bottom: 10px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-transform: ellipsis;
+ }
+ .spotify-playlists .list .item p{
+    color: #989898;
+    font-size: 12px;
+    line-height: 20px;
+    font-weight: 600;
+}
+
+
+
+
+
+<div class="list">
+      <div class="item">
+        <img src="https://i.pinimg.com/originals/a9/3c/b9/a93cb90a5618c6d77461dd534b5595af.png" />
+        <div class="play">
+          <span class="fa fa-play"></span>
+        </div>
+        <h4>Today's Top Hits</h4>
+        <p>Rema & Selena Gomez are on top of the...</p>
+      </div>
+
+
+      <div class="spotify-playlists">
+    <h2>Spotify Playlists</h2>
+
+    <div class="list">
+          <div class="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoWR0hBMsGgzbT8G5PaLDAPu5PdrO71EfqocFQoSjzqtK59Sq5wOrqIFpCS7keGKHf_GY&usqp=CAU" />
+            <div class="play">
+              <span class="fa fa-play"></span>
+            </div>
+            <h4>Today's Top Hits</h4>
+            <p>Rema & Selena Gomez are on top of the...</p>
+          </div>
+    
+
+      <div class="item">
+        <img src="https://i.scdn.co/image/ab67616d0000b2733b5e11ca1b063583df9492db" />
+       <div class="play">
+<span class="fa fa-play"></span>
+</div>
+<h4>RapCaviar</h4>
+<p>New Music from Lil Baby, Juice WRLD an...</p>
+</div>
+
+
+<div class="item">
+        <img src="https://i.scdn.co/image/ab67616d0000b2733b5e11ca1b063583df9492db" />
+       <div class="play">
+<span class="fa fa-play"></span>
+</div>
+<h4>RapCaviar</h4>
+<p>New Music from Lil Baby, Juice WRLD an...</p>
+</div>
+  c
+      
+  <div class="item">
+    <img src="https://i.scdn.co/image/ab67616d0000b2733b5e11ca1b063583df9492db" />
+    <div class="play">
+      <span class="fa fa-play"></span>
+    </div>
+    <h4>hdvxyfdhj</h4>
+    <p>...</p>
+  </div>
+      
+  <div class="item">
+    <img src="https://via.placeholder.com/150" />
+    <div class="play">
+      <span class="fa fa-play"></span>
+    </div>
+    <h4>All out 2010s</h4>
+    <p>The biggest spmgs pf tje 2010s. Cover:...</p>
+  </div>
+  </div>
+  
